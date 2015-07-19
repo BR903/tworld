460606c456af46070aee91dba31407d1cecbf259
Brian Raiter <breadbox@muppetlabs.com>
2015-07-19 16:11:37 -0700

    Trying to figure out how to cross-compile again.

--- win32/cross-configure.sh
+++ win32/cross-configure.sh
@@ -2,9 +2,9 @@
 
 CONFIG_SHELL=/bin/sh
 export CONFIG_SHELL
-PREFIX=/usr/local/cross-tools
-TARGET=i386-mingw32msvc
-PATH="$PREFIX/bin:$PREFIX/$TARGET/bin:$PATH"
+PREFIX=/usr
+TARGET=i586-mingw32msvc
+PATH="/$PREFIX/$TARGET/bin:$PATH"
 export PATH
 if [ -f "$PREFIX/$TARGET/bin/$TARGET-sdl-config" ]; then
     SDL_CONFIG="$PREFIX/$TARGET/bin/$TARGET-sdl-config"
