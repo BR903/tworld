7267bb1c412942b500abce45d2f6dfe5881e85a4
Brian Raiter <breadbox@muppetlabs.com>
2015-07-20 22:59:49 -0700

    Fixed incorrect comment.

--- oshw.h
+++ oshw.h
@@ -161,10 +161,15 @@ extern void cleardisplay(void);
 extern int displaygame(void const *state, int timeleft, int besttime);
 
 /* Display a short message appropriate to the end of a level's game
- * play. If the level was completed successfully, completed is TRUE,
- * and the other three arguments define the base score and time bonus
- * for the level, and the user's total score for the series; these
- * scores will be displayed to the user.
+ * play. If the level was completed successfully, totalscore is
+ * nonzero, and the other three arguments define the base score and
+ * time bonus for the level, and the user's total score for the
+ * series; these scores will be displayed to the user. completed
+ * controls the prompt that the user sees at the bottom of the
+ * display. A positive value will indicate that more text follows. A
+ * negative value will indicate that leaving this screen will return
+ * to the prior display. A value of zero will indicate that the
+ * current display is the end of a sequence.
  */
 extern int displayendmessage(int basescore, int timescore, long totalscore,
 			     int completed);
