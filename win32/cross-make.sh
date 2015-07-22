b814979ad7f3ab20c6cb9e67b585600e01180e44
Brian Raiter <breadbox@muppetlabs.com>
2015-07-22 11:22:07 -0700

    working on reviving cross compile

--- win32/cross-make.sh
+++ win32/cross-make.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-PREFIX=/usr
-TARGET=i586-mingw32msvc
-PATH="$PREFIX/$TARGET/bin:$PATH"
+PREFIX=/usr/local/cross-tools
+TARGET=i686-w64-mingw32msvc
+MSVCPREFIX=/usr/i586-mingw32msvc
+PATH="$PREFIX/bin:$PREFIX/$TARGET/bin:$MSVCPREFIX/bin:$PATH"
 export PATH
-exec make $*
+
+#CFLAGS = -I$MSVCPREFIX/include -I$PREFIX/$TARGET/include -mwindows
+#LDFLAGS = -L$MSVCPREFIX/lib -L$PREFIX/$TARGET/lib -mwindows
+#LOADLIBES = -lmingw32
+
+exec make "$*"
