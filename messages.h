f8508dd21cd6081db002781a6b95b1b4febaa22d
Brian Raiter <breadbox@muppetlabs.com>
2015-08-05 23:21:30 -0700

    documentation fixes

--- messages.h
+++ messages.h
@@ -10,12 +10,22 @@
 #include "defs.h"
 #include "gen.h"
 
-/* Read a file containing an array of tagged texts. Each tag is a
- * number, indicating the level for which the message is displayed. (A
- * negative number tags prolog text, a positive number tags epilog
- * text.) The return value is NULL if the file is empty or unreadable.
- * The caller must eventually free the data via the freetaggedtext()
- * function.
+/* Read a file containing an array of tagged message texts. Each tag
+ * is a number, indicating the level for which the message is
+ * displayed. The return value is NULL if the file is empty or
+ * unreadable. The caller must eventually free the data via the
+ * freetaggedtext() function.
+ *
+ * A message text is a sequence of paragraphs. The tag must appear
+ * before the text in square brackets, on a line by itself. If the tag
+ * is preceded by a minus sign, the message is marked as prologue
+ * text; if it is preceded by a plus sign, the message is epilogue
+ * text. (If no sign is present, epilogue text is assumed.) Within the
+ * message text, paragraphs are separated by blank lines, i.e. two or
+ * more line break characters in a row. (Line breaks at the beginning
+ * or end of a text are dropped.) Single line breaks within a
+ * paragraph are replaced with spaces. A literal line break can be
+ * indicated by ending a line with two spaces before the line break.
  */
 extern taggedtext *readmessagesfile(char const *filename);
 
